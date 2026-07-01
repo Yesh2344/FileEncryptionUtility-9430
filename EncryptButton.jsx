2c10742204651c313296ae00fda28452d75abe5d
Yesh2344 <125935833+Yesh2344@users.noreply.github.com>
2026-07-01 08:02:11 +0000

    more sensible defaults

--- EncryptButton.jsx
+++ EncryptButton.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function EncryptButton({ onClick }) {
   return (
 // minor polish
+// was easier to read this way
     <div>
       <button onClick={onClick}>Encrypt</button>
     </div>
